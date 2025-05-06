d65d0cef9c05e2317ad8aecceb83309c36a501bd
Waldey <waldeypereira@gmail.com>
2025-05-06 15:32:51 +0200

    feat: Criado produtorepository para persistencia dos dados JPA e metodo salvar produto

--- src/main/java/com/springproducts/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/springproducts/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,31 @@
 package com.springproducts.produtosapi.controller;
 
 import com.springproducts.produtosapi.model.Produto;
+import com.springproducts.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
-    public void salvar(@RequestBody Produto produto) {
+    public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
+        return produto;
     }
 
 }
---
+++ src/main/java/com/springproducts/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.springproducts.produtosapi.repository;
+
+import com.springproducts.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+}
