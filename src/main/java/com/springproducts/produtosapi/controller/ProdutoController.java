73836ed503fd55d1a0a1c7d1270f8741a0afdd8d
Waldey <waldeypereira@gmail.com>
2025-05-07 16:37:08 +0200

    feat: Adicionado buscar produto em produtocontroller

--- src/main/java/com/springproducts/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/springproducts/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.springproducts.produtosapi.model.Produto;
 import com.springproducts.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -51,6 +52,11 @@ public class ProdutoController {
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto) {
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNome(nome);
     }
 
 
--- src/main/java/com/springproducts/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/com/springproducts/produtosapi/repository/ProdutoRepository.java
@@ -3,6 +3,10 @@ package com.springproducts.produtosapi.repository;
 import com.springproducts.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
 
 }
